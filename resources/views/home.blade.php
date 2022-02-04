f40dcd86f82a7f0e59580f480fc9afdb1353b7da
ranabilal <ranabilal9526060@gmail.com>
2022-02-05 03:14:13 +0500

    arabic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-    Dashboard
+{{ __('Dashboard') }}
 @endsection
 @section('content')
     <div class="page-content-wrapper ">
@@ -13,7 +13,7 @@
                             <span class="mini-stat-icon"><i class="mdi mdi-cart-outline"></i></span>
                             <div class="mini-stat-info text-right text-white">
                                 <span class="counter">{{ $usersCount }}</span>
-                                Total User
+                                {{ __('Total User') }}
                             </div>
                         </div>
                     </div>
@@ -22,7 +22,7 @@
                             <span class="mini-stat-icon"><i class="mdi mdi-currency-usd"></i></span>
                             <div class="mini-stat-info text-right text-white">
                                 <span class="counter">{{ $doctorsCount }}</span>
-                                Total Doctors
+                                {{ __('Total Doctors') }}
                             </div>
                         </div>
                     </div>
@@ -31,7 +31,7 @@
                             <span class="mini-stat-icon"><i class="mdi mdi-cube-outline"></i></span>
                             <div class="mini-stat-info text-right text-white">
                                 <span class="counter">0</span>
-                                Total Revenue
+                                {{ __('Total Revenue') }}
                             </div>
                         </div>
                     </div>
@@ -40,7 +40,7 @@
                             <span class="mini-stat-icon"><i class="mdi mdi-currency-btc"></i></span>
                             <div class="mini-stat-info text-right text-white">
                                 <span class="counter">0</span>
-                                Issues
+                                {{ __('Issues') }}
                             </div>
                         </div>
                     </div>
@@ -53,15 +53,15 @@
                     <div class="col-12">
                         <div class="card m-b-20">
                             <div class="card-body">
-                                <h4 class="mt-0 m-b-15 header-title"> Latest Users</h4>
+                                <h4 class="mt-0 m-b-15 header-title">  {{ __('Latest Users') }}</h4>
                                 <div class="table-responsive">
                                     <table class="table table-hover m-b-0">
                                         <thead>
                                             <tr>
-                                                <th>Name</th>
-                                                <th>Email</th>
-                                                <th>Phone No</th>
-                                                <th>Role</th>
+                                                <th> {{ __('Name') }}</th>
+                                                <th> {{ __('Email') }}</th>
+                                                <th> {{ __('Phone No') }}</th>
+                                                <th> {{ __('Role') }}</th>
                                             </tr>
 
                                         </thead>
@@ -91,10 +91,10 @@
                             <div class="col-lg-6">
                                 <div class="card m-b-200">
                                     <div class="card-body">
-                                        <h4 class="mt-0 header-title" style="font-size: 20px">Pending Balance</h4>
+                                        <h4 class="mt-0 header-title" style="font-size: 20px"> {{ __('Pending Balance') }}</h4>
                                         <hr>
                                         <h1 class="text-center">{{ $user->wallet->pending_balance }}.00<span
-                                                style="color: #0d6efd!important"> USD</span>
+                                                style="color: #0d6efd!important">  {{ __('USD') }}</span>
                                         </h1>
 
                                     </div>
@@ -103,10 +103,10 @@
                             <div class="col-lg-6">
                                 <div class="card m-b-200">
                                     <div class="card-body">
-                                        <h4 class="mt-0 header-title" style="font-size: 20px">Available balance</h4>
+                                        <h4 class="mt-0 header-title" style="font-size: 20px"> {{ __('Available balance') }}</h4>
                                         <hr>
                                         <h1 class="text-center">{{ $user->wallet->available_balance }}.00 <span
-                                                style="color: #0d6efd!important"> USD</span>
+                                                style="color: #0d6efd!important">  {{ __('USD') }}</span>
                                         </h1>
 
                                     </div>
@@ -119,7 +119,7 @@
                             <div class="col-lg-12">
                                 <div class="card m-b-200">
                                     <div class="card-body">
-                                        <h4 class="mt-0 header-title" style="font-size: 20px">Withdraw balance
+                                        <h4 class="mt-0 header-title" style="font-size: 20px"> {{ __('Withdraw balance') }}
                                         </h4>
                                         <hr>
                                         <p class="text-muted m-b-20 font-14"></p>
@@ -130,30 +130,20 @@
                                                 <div class="col-lg-12">
                                                     <div class="form-group">
                                                         <h3 style=" " class="text-center"><strong
-                                                                style="color: #198754!important"> Available balance :
+                                                                style="color: #198754!important"> {{ __('Available balance') }}
                                                                 {{ $user->wallet->available_balance }}.00
-                                                                <span style="color: #0d6efd!important"> USD</span>
+                                                                <span style="color: #0d6efd!important">  {{ __('USD') }}</span>
                                                             </strong> </h3>
                                                         @if ($withDraw)
-                                                            <input
-                                                                style="    color: #664d03;
-                                                                                                                                                                                                                            background-color: #fff3cd;
-                                                                                                                                                                                                                       border-color: #ffecb5;"
-                                                                type="text" readonly class="form-control"
-                                                                name="offer_arabic" readonly
-                                                                placeholder="Pending withdrawal request 3428.00USD"
-                                                                value="Pending withdrawal request {{ $withDraw->withdraw_amount }}.00USD">
-
-
+                                                            <input  style="    color: #664d03;   background-color: #fff3cd;        border-color: #ffecb5;"  
+                                                             type="text" readonly class="form-control"  name="offer_arabic" readonly 
+                                                              placeholder="" 
+                                                               value="Pending withdrawal request {{ $withDraw->withdraw_amount }}.00USD">
                                                         @elseif ($user->wallet->available_balance >= 50)
                                                             <input class="form-control" type="number"
                                                                 name="withdraw_amount" id="">
                                                         @else
-                                                            <p class="text-center text-primary">Minimum 50$ available
-                                                                balance is
-                                                                required for withdraw</p>
-
-
+                                                            <p class="text-center text-primary"> {{ __('Minimum 50$ available balance is required for withdraw') }}</p>
                                                         @endif
 
                                                     </div>
@@ -166,7 +156,7 @@
                                                     <div>
                                                         <button type="submit"
                                                             class="btn btn-primary waves-effect waves-light">
-                                                            Withdraw
+                                                             {{ __('Withdraw') }}
                                                         </button>
                                                     </div>
                                                 </div>
@@ -217,14 +207,14 @@
                     <div class="col-6">
                         <div class="card m-b-20">
                             <div class="card-body">
-                                <h4 class="mt-0 m-b-15 header-title" style="font-size: 20px"> Debited logs</h4>
+                                <h4 class="mt-0 m-b-15 header-title" style="font-size: 20px">  {{ __('Debited logs') }}</h4>
                                 <div class="table-responsive">
                                     <table class="table table-hover m-b-0">
                                         <thead>
                                             <tr>
-                                                <th>Amount</th>
-                                                <th>Date</th>
-                                                <th>Status</th>
+                                                <th> {{ __('Amount') }}</th>
+                                                <th> {{ __('Date') }}</th>
+                                                <th> {{ __('Status') }}</th>
 
                                                 {{-- <th>Start date</th>
                                     <th>Salary</th> --}}
@@ -235,10 +225,10 @@
                                             @foreach ($user->payment_transaction_debited as $transaction)
                                                 <tr>
                                                     <td><span class="badge badge-primary">-{{ $transaction->amount }}.00
-                                                            USD</span>
+                                                             {{ __('USD') }}</span>
                                                     </td>
                                                     <td>{{ $transaction->created_at->toDateString() }}</td>
-                                                    <td>Debited</td>
+                                                    <td> {{ __('Debited') }}</td>
                                                 </tr>
                                             @endforeach
                                             @foreach ($user->withdraw as $withdraw)
@@ -246,7 +236,7 @@
                                                 <tr>
                                                     <td><span
                                                             class="badge badge-primary">-{{ $withdraw->withdraw_amount }}.00
-                                                            USD</span>
+                                                             {{ __('USD') }}</span>
                                                     </td>
                                                     <td>{{ $withdraw->created_at->toDateString() }}</td>
                                                     <td>{{ $withdraw->status }}</td>
@@ -265,15 +255,15 @@
                     <div class="col-6">
                         <div class="card m-b-20" style="max-height: 400px;overflow-y:auto;">
                             <div class="card-body">
-                                <h4 class="mt-0 m-b-15 header-title" style="font-size: 20px"> Balance log</h4>
+                                <h4 class="mt-0 m-b-15 header-title" style="font-size: 20px">  {{ __('Balance log') }}</h4>
                                 <div class="table-responsive">
                                     <table class="table table-hover m-b-0">
                                         <thead>
                                             <tr>
-                                                <th>Amount</th>
-                                                <th>Date</th>
-                                                <th>Status</th>
-                                                <th>Source</th>
+                                                <th> {{ __('Amount') }}</th>
+                                                <th> {{ __('Date') }}</th>
+                                                <th> {{ __('Status') }}</th>
+                                                <th> {{ __('Source') }}</th>
 
                                                 {{-- <th>Start date</th>
                                     <th>Salary</th> --}}
@@ -285,7 +275,7 @@
                                                 <tr>
                                                     <td><span class="badge badge-primary"
                                                             style="background: #198754!important">{{ $transaction->amount }}.00
-                                                            USD</span>
+                                                             {{ __('USD') }}</span>
                                                     </td>
 
                                                     <td>{{ $transaction->created_at->toDateString() }}</td>
@@ -293,9 +283,6 @@
                                                     <td>{{ $transaction->payment_type ?? 'appointment' }}</td>
                                                 </tr>
                                             @endforeach
-
-
-
                                         </tbody>
                                     </table>
                                 </div>
@@ -311,7 +298,7 @@
                             <span class="mini-stat-icon"><i class="mdi mdi-cart-outline"></i></span>
                             <div class="mini-stat-info text-right text-white">
                                 <span class="counter">0000</span>
-                                Coming Soon
+                                 {{ __('Coming Soon') }}
                             </div>
                         </div>
                     </div>
@@ -320,7 +307,7 @@
                             <span class="mini-stat-icon"><i class="mdi mdi-currency-usd"></i></span>
                             <div class="mini-stat-info text-right text-white">
                                 <span class="counter">0000</span>
-                                Coming Soon
+                                {{ __('Coming Soon ') }}
                             </div>
                         </div>
                     </div>
